cf5c7b8e4d06d7e5deb90d8ba375a52a6f7d3661
EVERTON DENEGA SOUZA <71551@PROWAY.TREINA>
2022-03-06 18:48:26 -0300

    exercicios final

--- exercicios aula3.sql
+++ exercicios aula3.sql
@@ -90,6 +90,15 @@ select * from pai;
 select * from mae;
 select * from filho;
 
+/* ----------------------------- 
+1 - Mostrar os nomes dos filhos e se possuir mãe, mostrar possui mãe, 
+--caso contrário mostrar não tem mãe, se possui pai, mostrar possui pai, 
+--caso contrário mostrar não tem pai.
+	--Resultado esperado:
+	--	Zézinho | Tem mãe 	  | Tem pai
+		--Tuca 	| Não tem mãe | Não tem pai*/
+		
+		
 select f.NOME as 'Nome Filho',
 case when f.MAE_CODIGO is null then 'Não tem Mãe' else 'Têm Mãe' end as 'Nome Mãe' ,
 case when f.PAI_CODIGO is null then 'Não tem Pai' else 'Têm Pai' end as 'Nome Pai'
@@ -121,7 +130,7 @@ inner join mae m on f.MAE_CODIGO = m.CODIGO
 left join pai p on f.PAI_CODIGO = p.CODIGO
 ;
 
-/*4 - Mostrar a quantidade de filhos que não possuem mãe ou não possuem pai.*/
+-- 4 - Mostrar a quantidade de filhos que não possuem mãe ou não possuem pai.*/
 select count(*)
 from filho f
 where f.MAE_CODIGO is null or f.PAI_CODIGO is null
@@ -143,3 +152,76 @@ m.NOME as 'Mãe'
 from filho f
 inner join mae m on f.MAE_CODIGO = m.CODIGO 
 order by m.CODIGO desc ;
+
+-- 7- Mostrar o nome dos filhos que tenham a mãe como Gabriela, Juliana (utilizando OR)
+select f.nome
+from filho f
+inner join mae m on f.MAE_CODIGO = m.CODIGO 
+where m.NOME = 'Gabriela' or m.NOME = 'Juliana';
+
+-- 8- Mostrar o nome dos filhos que não tenham o pai como Juca, Juliano (utilizando NOT IN)
+select f.nome
+from filho f
+inner join pai p on f.PAI_CODIGO = p.CODIGO 
+where p.NOME not in ('Juca','Juliano');
+
+-- exemplo de uso do UPPER e lower 
+select upper('silvia'),  lower('SILVIA');
+
+
+-- 9 - Mostrar os nomes dos pais que possuem mais de 1 filho.
+select p.nome as 'Pai',
+count(f.CODIGO) as 'Qtd Filhos'
+from filho f
+inner join pai p on f.PAI_CODIGO = p.CODIGO
+group by p.CODIGO
+having count(f.CODIGO) > 1;
+
+-- 10 - Mostrar os nomes dos pais e mães que possuem mais de 1 filho.
+select p.nome as 'Pai',
+m.NOME as 'Mae',
+count(f.CODIGO) as 'Qtd Filhos'
+from filho f
+inner join pai p on f.PAI_CODIGO = p.CODIGO
+inner join mae m on f.MAE_CODIGO = m.CODIGO
+group by m.NOME, p.NOME 
+having count(f.CODIGO) > 1;
+
+-- 11 - Mostrar o nome do pai e da mãe, mostrando a quantidade de filhos que tem juntos, 
+-- ordenando a consulta para trazer os que possuem mais filhos juntos.
+
+select p.nome as 'Pai',
+m.NOME as 'Mae',
+count(f.CODIGO) as 'Qtd Filhos'
+from filho f
+inner join pai p on f.PAI_CODIGO = p.CODIGO
+inner join mae m on f.MAE_CODIGO = m.CODIGO
+group by m.NOME, p.NOME
+order by count(f.CODIGO) desc ;
+
+-- 12 - Trazer apenas as mães que não tenham filhos
+insert into mae (codigo, nome ) values (6, 'Regina');
+
+select m.nome as 'Mae',
+count(*) as 'Qtd Filhos'
+from mae m 
+where not exists (select 1 from filho f where f.MAE_CODIGO = m.CODIGO)
+group by m.CODIGO 
+;
+
+-- 13 - Trazer os nomes dos pais, e outra coluna com a quantidade de caracteres que o nome do pai possui 
+--(Exemplo: SELECT LENGTH('aluno');
+
+select p.nome as 'Pai',
+length (p.nome) as 'Qtd Caracateres'
+from pai p;
+
+-- 14 - Executar o  abaixo e verificar o resultado. Tente entender o que a função UNION faz:
+SELECT FIL.NOME, 'Filho sem pai' AS PAI
+FROM FILHO FIL
+WHERE FIL.PAI_CODIGO IS null
+union
+SELECT FIL.NOME, 'Filho com pai' AS PAI
+FROM FILHO FIL
+WHERE FIL.PAI_CODIGO IS NOT null
+ORDER BY PAI;
